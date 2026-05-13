0ade42b3f2f8831f4eb8ee3193997ffbfb7d9324
Korada varun <koradavarun170@gmail.com>
2026-05-13 19:48:18 +0530

    fixed signin form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header/header";
-import Body from "./components/body/body";
 import LoginPage from "./components/loginpage/loginpage";
 
 function App() {
