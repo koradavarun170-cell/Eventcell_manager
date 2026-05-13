bf27d189d33c23a5a7e86ffb547c3d519dc814de
Korada varun <koradavarun170@gmail.com>
2026-05-13 20:43:24 +0530

    fixed signin form

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,11 @@ app.use("/api", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
-
+  
 app.listen(PORT, () => {
   console.log("server started at port:", PORT);
 });
